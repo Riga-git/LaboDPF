876567b38c1295c0734eb352253228dbb4fc492d
Lri <luca.rigazzi@gmail.com>
2021-04-20 20:59:32 +0200

    Clean up project

--- src/App.java
+++ src/App.java
@@ -2,17 +2,6 @@ import Fruits.Fruit;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Fruit orange = new Fruit("Orange", true);
-        Fruit banane = new Fruit("Banane", false);
-
-        orange.afficherFruit();
-        banane.afficherFruit();
         
-        orange.setNom("Orange sanguine");
-        orange.afficherFruit();
-
-        orange.setNom("banane speciale");
-        orange.setPepin(true);
-        orange.afficherFruit();
     }
 }
